0e11fde38240d31e6f303da093f1e3d5099b3341
Jayant <bhuniajayant@gmail.com>
2025-09-18 19:08:21 +0000

    chnages

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/dashboard', [DashboardController::class, "index"])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [DashboardController::class, "index"])->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
