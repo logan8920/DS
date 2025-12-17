f41821a2f63acd5249295ea5d962a97bb5de6cd5
Jayant <bhuniajayant@gmail.com>
2025-12-18 03:11:36 +0530

    changes

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'shopify' => [
+        'webhook_secret' => env('SHOPIFY_WEBHOOK_SECRET'),
+    ],
 
 ];
