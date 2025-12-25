17091fa65bdba76f586a380971cde44ae10c29c5
Jayant <bhuniajayant@gmail.com>
2025-12-26 01:56:18 +0530

    changes

--- routes/webhook.php
+++ routes/webhook.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\ShopifyWebhookController;
 Route::prefix('shopify')->group(function(){
-    Route::post('/api/shopify/webhooks', [ShopifyWebhookController::class, 'handle']);
+    Route::post('/webhooks', [ShopifyWebhookController::class, 'handle']);
     Route::post('/webhooks/customers/data_request', [ShopifyWebhookController::class, 'customerDataRequest']);
     Route::post('/webhooks/customers/redact', [ShopifyWebhookController::class, 'customerRedact']);
     Route::post('/webhooks/shop/redact', [ShopifyWebhookController::class, 'shopRedact']);
