7e4dbc57b0c217de6ec7fd09750e2f8ac431ec5e
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-04-19 01:56:03 +0530

    home changes

--- routes/channels.php
+++ routes/channels.php
@@ -1,12 +1,12 @@
 <?php 
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use App\Http\Controllers\ChannelsController;
 
 Route::prefix('channels')->group(function(){
     Route::get("/allChannels",[ChannelsController::class, "index"])->name("channels.allChannels");
-    Route::post("/get/allChannels",[ChannelsController::class, "show"])->name("channels.allChannels.get");
+    Route::post("/get/allChannels",[ChannelsController::class, "show"])->name("channels.allChannels.get")->withoutMiddleware([VerifyCsrfToken::class]);
     Route::get("/addChannels",[ChannelsController::class,"create"])->name("channels.create");
     Route::post("create/channels",[ChannelsController::class, "store"])->name("channels.store");
 });
