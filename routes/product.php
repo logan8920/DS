52e5e3575e452f0a76e584ddc79efad999e36291
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-04-20 23:39:01 +0530

    home changes

--- routes/product.php
+++ routes/product.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
 
 Route::prefix("product")->group(function () {
     Route::get("{product}/details",[ProductController::class, "details"])->name("product.details");
     Route::get("/categories",[ProductController::class, "categories"])->name("product.categories");
     Route::get("/category/{parent}/{id}",[ProductController::class, "categoryProductShow"])->name("product.category.show");
-    Route::post("push-to-shopify",[ProductController::class, "pushToShopify"])->name("product.push");
+    Route::post("push-to-shopify",[ProductController::class, "pushToShopify"])->name("product.push")->withoutMiddleware([VerifyCsrfToken::class]);
 });
