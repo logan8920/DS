035e5efebeaf1f9d6aff64b5d46c7ff981e71222
Jayanta Bhunia <jayanta.bhunia@acefone.com>
2026-04-21 00:10:15 +0530

    home changes

--- routes/channels.php
+++ routes/channels.php
@@ -8,5 +8,5 @@ Route::prefix('channels')->group(function(){
     Route::get("/allChannels",[ChannelsController::class, "index"])->name("channels.allChannels");
     Route::post("/get/allChannels",[ChannelsController::class, "show"])->name("channels.allChannels.get")->withoutMiddleware([VerifyCsrfToken::class]);
     Route::get("/addChannels",[ChannelsController::class,"create"])->name("channels.create");
-    Route::post("create/channels",[ChannelsController::class, "store"])->name("channels.store");
+    Route::post("create/channels",[ChannelsController::class, "store"])->name("channels.store")->withoutMiddleware([VerifyCsrfToken::class]);
 });
